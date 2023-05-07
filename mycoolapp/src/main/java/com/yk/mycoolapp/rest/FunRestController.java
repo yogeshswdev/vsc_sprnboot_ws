fbdd7e7b6985acf618d64860509b1f45ace4f6a2
Yogesh <yogeshkhot2007@gmail.com>
2023-05-07 11:15:39 +0530

    added input param

--- mycoolapp/src/main/java/com/yk/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/yk/mycoolapp/rest/FunRestController.java
@@ -2,14 +2,15 @@ package com.yk.mycoolapp.rest;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class FunRestController {
     // expose sample endpoint which return hello world
 
     @GetMapping(value = "/")
-    public String sayHello() {
-        return "Hello world";
+    public String sayHello(@RequestParam(value = "name", defaultValue = "YK") String name) {
+        return "Hello world " + name;
     }
 
 }
